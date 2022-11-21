83901b4ef8cdb8c4795cd32cfb6c143fa7d9a051
Patrick Maloney <patrick@foobar.agency>
2022-11-21 10:48:29 +0100

    rename dir

---
+++ lua/pmalo/settings.lua
@@ -0,0 +1,18 @@
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.textwidth = 80
+vim.opt.expandtab = true
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+vim.opt.ignorecase = true
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+vim.opt.signcolumn = 'yes'
+vim.opt.wrap = false
+vim.g.mapleader = " "
+vim.g.netrw_bufsettings = 'nu relativenumber'
+vim.g.netrw_banner = 0
