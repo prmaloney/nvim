3e91582c7da666cad5bb1f530946cda8b9691072
Patrick Maloney <patrick@foobar.agency>
2022-11-22 14:21:30 +0100

    remove unused terminal map

--- nvim/after/plugin/toggleterm.lua
+++ nvim/after/plugin/toggleterm.lua
@@ -33,5 +33,3 @@ function _lazygit_toggle()
 end
 
 vim.api.nvim_set_keymap("n", "<leader>gg", "<cmd>lua _lazygit_toggle()<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>t", "<cmd>ToggleTerm direction=vertical size=50<CR>",
-  { noremap = true, silent = true })
