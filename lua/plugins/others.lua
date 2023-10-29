4ce1bf3f99eaa2044dc23c9c76e1cda8ce613e9e
Patrick Maloney <patrick@foobar.agency>
2023-10-29 17:30:34 +0100

    dont use after

--- lua/plugins/others.lua
+++ lua/plugins/others.lua
@@ -1,14 +1,5 @@
 return {
     'tpope/vim-fugitive',
     'tpope/vim-surround',
-    {
-        'stevearc/oil.nvim',
-        opts = {
-            default_file_explorer = true,
-            view_options = {
-                show_hidden = true
-            }
-        }
-    },
     { 'numToStr/Comment.nvim', opts = {} },
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,6 +3,8 @@ return {
     'nvim-treesitter/nvim-treesitter',
     dependencies = {
         'nvim-treesitter/nvim-treesitter-textobjects',
+        'nvim-treesitter/playground',
+        'nvim-treesitter/nvim-treesitter-context'
     },
     build = ':TSUpdate',
     config = function()
