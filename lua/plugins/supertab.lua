fd5f58ad7d4f454b5bfabf457d1b1e1a106c5edc
prmaloney <pmaloney16@gmail.com>
2026-02-04 10:00:43 +0100

    how about blink?

--- lua/plugins/supertab.lua
+++ lua/plugins/supertab.lua
@@ -1,6 +1,7 @@
 return {
-    "hrsh7th/nvim-cmp",
+    'saghen/blink.cmp',
     dependencies = {
+        'rafamadriz/friendly-snippets',
         {
             "zbirenbaum/copilot.lua",
             dependencies = {
@@ -26,58 +27,45 @@ return {
             end,
         },
     },
-    event = "InsertEnter",
-    config = function()
-        local cmp = require("cmp")
-        cmp.setup({
-            mapping = {
-                ["<Tab>"] = cmp.mapping(function(fallback)
-                    local copilot = require("copilot.suggestion")
-                    if copilot.is_visible() then
-                        copilot.accept()
-                    elseif cmp.visible() then
-                        local entry = cmp.get_selected_entry()
-                        if not entry then
-                            cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
-                        end
-                        cmp.confirm()
-                    else
-                        fallback()
+    version = '1.*',
+    opts = {
+        keymap = {
+            preset = 'enter',
+            ['<Tab>'] = {
+                'snippet_forward',
+                function()
+                    local copilot_suggestion = require("copilot.suggestion")
+                    if copilot_suggestion.is_visible() then
+                        copilot_suggestion.accept()
+                        return true
                     end
-                end, { "i", "s" }),
-                ["<S-Tab>"] = cmp.mapping(function(fallback)
-                    local copilot = require("copilot.suggestion")
-                    if copilot.is_visible() then
-                        copilot.next()
-                    elseif cmp.visible() then
-                        cmp.select_prev_item()
-                    else
-                        fallback()
+                end,
+                'fallback'
+            },
+            ['<S-Tab>'] = {
+                'snippet_backward',
+                function()
+                    local copilot_suggestion = require("copilot.suggestion")
+                    if copilot_suggestion.is_visible() then
+                        copilot_suggestion.next()
+                        return true
                     end
-                end, { "i", "s" }),
-                ["<C-p>"] = cmp.mapping.select_prev_item(),
-                ["<C-n>"] = cmp.mapping.select_next_item(),
-                ["<Up>"] = cmp.mapping.select_prev_item(),
-                ["<Down>"] = cmp.mapping.select_next_item(),
-                ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-                ["<C-f>"] = cmp.mapping.scroll_docs(4),
-                ["<C-Space>"] = cmp.mapping.complete(),
-                ["<C-e>"] = cmp.mapping.close(),
-                ["<CR>"] = cmp.mapping.confirm({ select = true }),
-            },
-            view = {
-                entries = { name = "custom" },
-            },
-            window = {
-                completion = cmp.config.window.bordered(),
-                documentation = cmp.config.window.bordered(),
-            },
-            sources = cmp.config.sources({
-                { name = "nvim_lsp" },
-                { name = "lazydev",              group_index = 0 },
-                { name = "vim-dadbod-completion" },
-                { name = "buffer" },
-            }),
-        })
-    end,
+                end,
+                'fallback'
+            }
+        },
+
+        appearance = {
+            nerd_font_variant = 'mono'
+        },
+
+        completion = { documentation = { auto_show = false } },
+
+        sources = {
+            default = { 'lsp', 'path', 'snippets', 'buffer' },
+        },
+
+        fuzzy = { implementation = "prefer_rust_with_warning" }
+    },
+    opts_extend = { "sources.default" }
 }
